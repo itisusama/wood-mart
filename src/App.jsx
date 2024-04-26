c5d4560f82776d1747e95f095d5a7f3d544a0bcf
usama <ahmarkhan1233456@gmail.com>
2024-04-26 14:24:34 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
