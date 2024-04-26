223819ddd3cf852267a68a42311221ec5b0d89a5
usama <ahmarkhan1233456@gmail.com>
2024-04-26 15:55:21 +0500

    contact and about page update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,56 @@
 import React from 'react';
 import BreadCrumb from '../components/BreadCrumb';
 import { Container } from 'react-bootstrap';
+import BannerSection from '../components/Banner';
 
 const About = () => {
+
+  // Define an array of blog objects
+  const blogs = [
+    {
+      date: '26 April 2024 - Friday',
+      title: 'Blog Title 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, laudantium.',
+      imageUrl: 'https://via.placeholder.com/350x500',
+    },
+    {
+      date: '26 April 2024 - Friday',
+      title: 'Blog Title 2',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, laudantium.',
+      imageUrl: 'https://via.placeholder.com/350x500',
+    },
+    {
+      date: '26 April 2024 - Friday',
+      title: 'Blog Title 3',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, laudantium.',
+      imageUrl: 'https://via.placeholder.com/350x500',
+    },
+  ];
+
   return (
     <>
-     <BreadCrumb pageName='About Us'/> 
-     {/* Our Blog */}
-      <Container>
-        <h3>Read Our Blogs</h3>
-        <div className='mt-3'>
-          <img src="https://via.placeholder.com/200" alt="Placeholder Image" className="img-fluid"/>
-        
-        </div>
+      <BreadCrumb pageName='About Us'/> 
+      {/* Our Blog */}
+      <Container className='mt-4'>
+        <h3 className='text-center'>Read Our Blogs</h3>
+        <section className='d-flex justify-content-around'>
+          {/* Map through the blogs array */}
+          {blogs.map((blog, index) => (
+            <div className='mt-3' key={index}>
+              <img src={blog.imageUrl} alt="Placeholder Image" className="img-fluid"/>
+              <div>
+                <h4 className='mt-3'>{blog.date}</h4>
+                <h3>{blog.title}</h3>
+                <p style={{width:'300px'}} className='text-justify'>{blog.content}<br /> <a href="#">Read More</a></p>
+              </div>
+            </div>
+          ))}
+        </section>
       </Container>
-     {/* Our Blog */}
+      {/* Our Blog */}
+      <BannerSection/>
     </>
   )
 }
 
-export default About
+export default About;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,90 +6,56 @@ import img1 from '../assets/images/img1.jpg';
 const Contact = () => {
   return (
     <>
-    <BreadCrumb pageName= 'Contact Page' breadCrumbLink='/contact'/>
+      <BreadCrumb pageName='Contact Page' breadCrumbLink='/contact' />
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <h4 className="mb-3">Contact Us</h4>
+            <h4 className="mb-3" style={{fontStyle:'fw-semibold ', fontSize:'30px'}}>GOT ANY QUESTIONS?</h4>
+            <p className="mb-4">
+              Please fill out the form below to send us a message. We'll get back to you as soon as possible.
+            </p>
+            <div className="row">
+              <div className="col-6">
+                <h5>Office</h5>
+                <p>1316 Abbot Kinney Blvd.</p>
+                <p>Copenhagen UK 90291</p>
+                <p>+112 345 6789</p>
+                <p>Woodworth@example.com</p>
+              </div>
+              <div className="col-6">
+                <h5>Store</h5>
+                <p>1316 Abbot Kinney Blvd.</p>
+                <p>Copenhagen CA 90291</p>
+                <p>+112 345 6789</p>
+                <p>Woodworth@example.com</p>
 
-    <Container className='mt-3'>
-        <h2 className='text-center text-primary'>Heading Goes Here</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis eum aliquid dolorem laboriosam iste, fuga blanditiis minima a voluptatem adipisci fugit eveniet ipsum expedita doloribus, quasi tenetur optio culpa vel eos, sunt perferendis cupiditate at explicabo? Sint repellat praesentium mollitia necessitatibus beatae repellendus inventore veritatis velit, laborum, unde temporibus corrupti!</p>
-    </Container>
-    
-    <Container>
-                <Row>
-                    <Col md={4} className='border p-4 shadow-sm rounded'>
-                        <h5 className='text-primary'>Corporate Office</h5>
-                        <p>
-                            702 Main St, West Columbia <br />
-                            South Carolina, 29170 <br />
-                            United States
-                        </p>
-                    </Col>
-                    <Col md={4} className='border p-4 shadow-sm rounded'>
-                        <h5 className='text-primary'>Get in Touch</h5>
-                        <p>
-                            <b>+44 1234 567758</b> <br />
-                            person@company.com <br />
-                            person@company.com
-                        </p>
-                    </Col>
-                    <Col md={4} className='border p-4 shadow-sm rounded'>
-                        <h5 className='text-primary'>Customer Support</h5>
-                        <p>
-                            <b>Mon to Sat:</b>  09:00am - 08:00pm <br />
-                            <b>Sunday:</b>  10:00am - 06:00pm
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
-
-            <Container className="py-5">
-                <Row>
-                    <Col lg={6}>
-                        <h2 data-aos='fade-right' className='text-primary'>Contact Us</h2>
-                        <p data-aos='fade-right'>Have a question or want to get in touch? Fill out the form below:</p>
-                        <Form data-aos='fade-right'>
-                            <Form.Group controlId="formName">
-                                <Form.Label>Name</Form.Label>
-                                <Form.Control type="text" placeholder="Enter your name" />
-                            </Form.Group>
-                            <Form.Group controlId="formEmail">
-                                <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" placeholder="Enter your email" />
-                            </Form.Group>
-                            <Form.Group controlId="formMessage">
-                                <Form.Label>Message</Form.Label>
-                                <Form.Control as="textarea" rows={5} placeholder="Enter your message" />
-                            </Form.Group>
-                            <Button variant="primary" type="submit" className='mt-3 btn btn-primary'>Submit</Button>
-                        </Form>
-                    </Col>
-                    <Col lg={6} data-aos='fade-left' className="d-flex align-items-center justify-content-center mt-2">
-                        <img src={img1} alt="Contact" className='rounded' style={{ maxWidth: '100%', maxHeight: '100%' }} />
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md={12} className='mt-5'>
-                    <div className='row text-center justify-content-center' data-aos='fade-right'>
-                        <div className="col-md-11 col-lg-9 col-xl-7 mb-2">
-                            <div className="section-title">
-                                <h2 className="sec-title">Our Location</h2>
-                            </div>
-                        </div>
-                    </div>
-                        <div style={{ width: '100%', height: '300px', border: '1px solid #ddd' }}>
-                            <iframe
-                                title="Google Map"
-                                data-aos="flip-up"
-                                width="100%"
-                                height="100%"
-                                frameBorder="0"
-                                style={{ border: 0 }}
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12345.678901234567!2d-71.059773!3d42.360940!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDQ4JzU1LjEiTiA3McKwMjAnMTQuMyJX!5e0!3m2!1sen!2sus!4v1234567890123!5m2!1sen!2sus"
-                                allowFullScreen
-                            ></iframe>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 mt-5 mb-5">
+            <form>
+              <div className="row mb-3">
+                <div className="col">
+                  <label htmlFor="name" className="form-label"></label>
+                  <input type="text" style={{fontStyle:'italic'}} className="form-control"  id="name" placeholder="Name" />
+                </div>
+                <div className="col">
+                  <label htmlFor="email" className="form-label"></label>
+                  <input type="email" style={{fontStyle:'italic'}} className="form-control" id="email" placeholder="Email" />
+                </div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="comments" className="form-label"></label>
+                <textarea className="form-control" style={{fontStyle:'italic'}} id="comments" rows="7" placeholder="Comments" defaultValue={""}></textarea>
+              </div>
+              <div className="text-right">
+                <button type="button" style={{ backgroundColor: '#80bfb3', outline: 'none' }} className="btn btn-primary ml-auto">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
