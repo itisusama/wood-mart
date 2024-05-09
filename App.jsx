cf1aee11e7fa464e36a434b0bd8001c7ebecca87
Mehwishmushtaq <91072315+Mehwishmushtaq@users.noreply.github.com>
2024-05-09 12:26:45 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Footer from './components/Footer';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import AllProducts from './pages/AllProducts';
+import SingleProduct from './pages/SingleProduct'
+import Faq from './pages/Faq';
+import SingleBlog from './pages/SingleBlog';
+import Blog from './pages/Blog';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+const App = () => {
+//   useEffect(() => {
+//     AOS.init({ duration: 1000 });
+// }, []);
+
+  return (
+    <>
+      <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/allproducts" element={<AllProducts/>} />
+        <Route path="/singleproduct" element={<SingleProduct/>} />
+        <Route path="/faq" element={<Faq/>} />
+        <Route path="/single-blog" element={<SingleBlog/>} />
+        <Route path="/blog" element={<Blog/>} />
+      </Routes>
+      <Footer/>
+    </Router>
+    </>
+  )
+}
+
+export default App
